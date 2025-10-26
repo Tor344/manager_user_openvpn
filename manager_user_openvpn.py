7c045ced3e81b56ff987248e41607a2dd66c8c33
tor344 <tor@azgard@icloud.com>
2025-10-26 07:35:04 +0300

    fix: make main path "EASY_RSA_PATH", add input for subprocess.run

--- manager_user_openvpn.py
+++ manager_user_openvpn.py
@@ -32,22 +32,26 @@ verb 3
 </key>
 """
 
+EASY_RSA_PATH = "/etc/openvpn/easy-rsa"
 
 def add_user(name_user:str)->str:
+    try:
+        subprocess.run(["easyrsa", "gen-req", name_user, "nopass",], cwd=EASY_RSA_PATH, input= b"\n")
+        subprocess.run(["easyrsa", "sign-req", "client", name_user, ], cwd=EASY_RSA_PATH, input= b"yes")
 
-    subprocess.run(["easyrsa", "gen-req", name_user, "nopass", "/etc/openvpn/easy-rsa"])
-    subprocess.run(["easyrsa", "sign-req", "client", name_user, "/etc/openvpn/easy-rsa"])
+        with open("/etc/openvpn/easy-rsa/pki/ca.crt", "r") as f:
+            ca = f.read()
+        with open(f"/etc/openvpn/easy-rsa/pki/issued/{name_user}.crt","r") as f:
+            cert = f.read()
+        with open(f"/etc/openvpn/easy-rsa/pki/private/{name_user}.key") as f:
+            key = f.read()
 
-    with open("/etc/openvpn/easy-rsa/pki/ca.crt", "r") as f:
-        ca = f.read()
-    with open(f"/etc/openvpn/easy-rsa/pki/issued/{name_user}.crt","r") as f:
-        cert = f.read()
-    with open(f"/etc/openvpn/easy-rsa/pki/private/{name_user}.key") as f:
-        key = f.read()
+        data_openvpn = TEMPLATES.format(ca=ca, cert=cert, key=key)
+        return data_openvpn
 
-    data_openvpn = TEMPLATES.format(ca=ca, cert=cert, key=key)
-    return data_openvpn
-
+    except Exception as e:
+        print(e)
+        return ""
 
 def delete_user(name_user:str)->None:
     pass
